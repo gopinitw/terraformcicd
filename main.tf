705a5a25b5c86ef283a45f2b36a917fa85613d83
gopinitw <44080162+gopinitw@users.noreply.github.com>
2024-04-02 09:18:55 +0530

    samsung and cicd new bucket deleting

--- main.tf
+++ main.tf
@@ -35,35 +35,3 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Development"
   }
 }
-
-resource "aws_s3_bucket" "new_bucket" {
-  bucket = "well-tech-tf-cicd-new"  # Set your desired bucket name
-  acl    = "private"  # Set the access control list (ACL), options are private, public-read, public-read-write, or authenticated-read
-
-  # Optional: Configure versioning
-  versioning {
-    enabled = true
-  }
-
-  # Optional: Configure bucket tags
-  tags = {
-    Name = "MyBucket"
-    Environment = "Development"
-  }
-}
-
-resource "aws_s3_bucket" "samsung" {
-  bucket = "well-tech-jenkins-samsung"  # Set your desired bucket name
-  acl    = "private"  # Set the access control list (ACL), options are private, public-read, public-read-write, or authenticated-read
-
-  # Optional: Configure versioning
-  versioning {
-    enabled = true
-  }
-
-  # Optional: Configure bucket tags
-  tags = {
-    Name = "MyBucket"
-    Environment = "Development"
-  }
-}
